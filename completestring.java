b94e271a504da0560438a35ee9c58f1729ab33dd
Madhunisa <nisasenthil97@gmail.com>
2017-07-13 10:07:58 +0530

    Update and rename README.md to completestring.java

---
+++ completestring.java
@@ -0,0 +1,28 @@
+# program
+import java.io.*;
+import java.util.*;
+public class completestring
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        String ss=s.nextLine();
+        String s1=ss.toLowerCase();
+        ArrayList<String> a=new ArrayList<String>();
+        ArrayList<String> b=new ArrayList<String>();
+        for(char i='a';i<='z';i++)
+        {
+           a.add(Character.toString(i)); 
+        }
+        for(int j=0;j<s1.length();j++)
+      {
+          char  k=s1.charAt(j);
+          b.add(Character.toString(k));
+      }
+        
+    if(b.containsAll(a))
+        System.out.print("is an panagram");
+    else
+        System.out.print("not an panagram");
+    }
+}
